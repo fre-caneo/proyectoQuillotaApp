77f6969585b0f4de1f4f39ed3b7846fd0415d91f
fre-caneo <fre.caneo@duocuc.cl>
2023-09-13 16:54:23 -0300

    primera creacion de cards

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  tasks: Task[] = [
+    {
+      id: '1',
+      title: 'Primera Queja',
+      description: 'Primera queja del municipio',
+      items : [
+        {name: 'Actividad 1', completed: true},
+        {name: 'Actividad 2', completed: false},
+        {name: 'Actividad 3', completed: false},
+
+      ]
+    },
+    {
+      id: '2',
+      title: 'Primera Sugerencia del municipio',
+      description: 'Primera sugerencia del municipio',
+      items : [
+        {name: 'Actividad 1', completed: true},
+        {name: 'Actividad 2', completed: false},
+        {name: 'Actividad 3', completed: false},
+      ]
+    },
+    {
+      id: '3',
+      title: 'Primera Peticion',
+      description: 'Primera Peticion del municipio',
+      items : [
+        {name: 'Actividad 1', completed: true},
+        {name: 'Actividad 2', completed: false},
+        {name: 'Actividad 3', completed: false},
+      ]
+    },
+  ]
 
   constructor() { }
 
